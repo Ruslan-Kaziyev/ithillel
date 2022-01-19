6b9a6a678cb68f205032ed7a504059ecf849c266
Ruslan-Kaziyev <98050132+Ruslan-Kaziyev@users.noreply.github.com>
2022-01-19 21:43:24 +0200

    lesson1

---
+++ homework1.py
@@ -0,0 +1,17 @@
+first=10
+
+second=30
+
+print(first + second)   #Вывод суммы чисел
+
+print(first - second)   #       разность
+
+print(first * second)   #       произведение
+
+print(first / second)   #       результат деления
+
+print(first ** second)  #       возведение в степень
+
+print(first // second)  #       целочисленое деление
+
+print(first % second)   #       остаток от деления
