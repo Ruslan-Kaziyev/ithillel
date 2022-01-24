3c8ce32ec74d7a916b2126cb786ea59da6fe4c2f
Ruslan-Kaziyev <98050132+Ruslan-Kaziyev@users.noreply.github.com>
2022-01-24 19:56:49 +0200

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,19 @@
+########################### Вывод удвоенного значения целого числа
+
+print(2 * int(input('Please enter integer number:')))
+
+############################ Вывод удвоенного значения числа с точкой
+
+print(2 * float(input('Please enter float number:')))
+
+############################ Вывод удвоенной строки
+
+ent_str = input('Please enter string:' )
+
+print(ent_str + ent_str)
+
+print(ent_str * 2)
+
+print(f'{ent_str}{ent_str}')
+
+print('{}{}'.format(ent_str,ent_str))
